e97ac83c48c53ffe43ff9633ef93af85d032942a
FrederikRoeder <Frederik.Roeder@web.de>
2019-09-08 16:20:02 +0200

    CallListener Logs hinzugefügt

--- app/src/main/java/de/fhws/smartdisplay/services/CallListener.java
+++ app/src/main/java/de/fhws/smartdisplay/services/CallListener.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import de.fhws.smartdisplay.server.ConnectionFactory;
 import de.fhws.smartdisplay.server.ServerConnection;
@@ -14,25 +15,39 @@ import retrofit2.Response;
 
 public class CallListener extends BroadcastReceiver {
 
-
-
     @Override
     public void onReceive(final Context context, Intent intent) {
 
         SharedPreferences settings = context.getSharedPreferences("Settings", 0);
         ServerConnection serverConnection = new ConnectionFactory().buildConnection();
 
+
+        Log.d("pack", "Call vor if");
+
+
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
         if (TelephonyManager.EXTRA_STATE_RINGING.equals(state) && settings.getBoolean("NotificationState", false)) {
+
+
+            Log.d("pack", "Call in if");
+
 
             serverConnection.sendNotification("Telefon", settings.getString("Name", "")).enqueue(new Callback<Void>() {
                 @Override
                 public void onResponse(Call<Void> call, Response<Void> response) {
 
+
+                    Log.d("pack", "Call onResponse");
+
+
                 }
 
                 @Override
                 public void onFailure(Call<Void> call, Throwable t) {
+
+
+                    Log.d("pack", "Call onFailure");
+
 
                 }
             });
